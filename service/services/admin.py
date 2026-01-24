0b3dd0b31a811b74ee7fb11c61074969d01f2226
l9l9n_PC <l9l9ndos@gmail.com>
2026-01-25 00:58:34 +0600

    Day 1-2

--- service/services/admin.py
+++ service/services/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from services.models import Service, Plan, Subscription
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Plan)
+class PlanAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    pass
--- service/services/models.py
+++ service/services/models.py
@@ -1,3 +1,35 @@
+from django.core.validators import MaxValueValidator
 from django.db import models
 
-# Create your models here.
+from clients.models import Client
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    full_price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"Service: {self.name} Price: {self.full_price}"
+
+
+class Plan(models.Model):
+    PLAN_TYPES = (
+        ('full', 'Full'),
+        ('student', 'Student'),
+        ('discount', 'Discount'),
+    )
+
+    plan_type = models.CharField(max_length=100, choices=PLAN_TYPES)
+    discount_percent = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(100)])
+
+    def __str__(self):
+        return f"Plan: {self.plan_type} Discount: {self.discount_percent}   "
+
+
+class Subscription(models.Model):
+    client = models.ForeignKey(Client, related_name='subscriptions' ,on_delete=models.PROTECT)
+    service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.CASCADE)
+    plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.client} | {self.service} | {self.plan}"
