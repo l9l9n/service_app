b569d734b995cc8dbfc52a7221e1fe2b54a1b585
l9l9n_PC <l9l9ndos@gmail.com>
2026-01-27 02:23:41 +0600

    Annotate

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -1,13 +1,26 @@
 from rest_framework import serializers
 
-from services.models import Subscription
+from services.models import Subscription, Plan
+
+
+class PlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plan
+        fields = ('plan_type', 'discount_percent')
+
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
+    plan = PlanSerializer()
     client_name = serializers.CharField(source='client.company_name', read_only=True)
     email = serializers.CharField(source='client.user.email', read_only=True)
+    price = serializers.SerializerMethodField()
+
+    def get_price(self, instance):
+        return instance.service.full_price - instance.service.full_price * (
+                instance.plan.discount_percent / 100)
 
     class Meta:
         model = Subscription
-        fields = ('id', 'plan_id', 'client_name', 'email')
+        fields = ('id', 'plan_id', 'client_name', 'email', 'plan', 'price')
 
